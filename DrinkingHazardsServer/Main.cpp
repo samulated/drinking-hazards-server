3000dc19ab767d06700bf512e393edfe199df5c5
samulated <samuel.fawcett@gmail.com>
2017-10-19 23:05:08 +1100

    First Commit

---
+++ DrinkingHazardsServer/Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+	bool running = true;
+
+	std::cout << "=== Drinking Hazards Server Program ===";
+	std::cout << "Initialising...";
+	std::cout << "...";
+
+/*	while (running)
+	{
+
+	}					*/
+
+	std::cout << "Shutting down. Thank you for playing!";
+
+}
